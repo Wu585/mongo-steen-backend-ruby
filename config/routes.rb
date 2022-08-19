ef7875ac8f97a20917cf00a49f42406d6f19142f
RickyWu <15151851516@163.com>
2022-08-19 23:16:41 +0800

    创建路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  post "/users", to: "users#create"
-  get "user/:id", to: "users#show"
+  namespace :api do
+    namespace :v1 do
+      # /api/v1
+      resources :validation_codes, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
+      resources :items
+      resources :tags
+    end
+  end
 end
